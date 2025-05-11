d8990ca15f27c4107f0f4289785a6fb4a0146401
Rani <sneha.d.rani@capgemini.com>
2025-05-11 18:19:37 +0530

    added image using property binding

--- src/assets/dummy-server.ts
+++ src/assets/dummy-server.ts
@@ -1,21 +1,25 @@
 export interface Server{
     serverId : number;
     serverName: string;
+    serverImageUrl: string;
 } 
 
 export const servers: Server[] = [
     {
         serverId : 1,
-        serverName : "Server1"
+        serverName : "Server1",
+        serverImageUrl : '../../../assets/server image.jpg'
     },
 
     {
         serverId : 2,
-        serverName : "Server2"
+        serverName : "Server2",
+        serverImageUrl : '../../../assets/server image.jpg'
     },
 
     {
         serverId : 3,
-        serverName : "Server3"
+        serverName : "Server3",
+        serverImageUrl : '../../../assets/server image.jpg'
     }
 ]
