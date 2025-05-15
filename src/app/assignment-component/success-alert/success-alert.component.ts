4c44592d0f1c3f48ae43e49ee7a271b5b99c0d59
Rani <sneha.d.rani@capgemini.com>
2025-05-15 10:54:02 +0530

    added computed shinal

--- src/app/assignment-component/success-alert/success-alert.component.ts
+++ src/app/assignment-component/success-alert/success-alert.component.ts
@@ -1,24 +1,32 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import {servers} from '../../../assets/dummy-server'
 
 // It will give the random index value from 0 to the length of the server array.
-const index : number = Math.floor(Math.random() * servers.length);
+const index : number = Math.floor(Math.random() * servers.length); 
 @Component({
   selector: 'app-success-alert',
   templateUrl: './success-alert.component.html',
   styleUrl: './success-alert.component.css'
 })
-
+  
 export class SuccessAlertComponent {
   // initializing a signal
-  selectedServer = signal(servers[index]);
+selectedServer = signal(servers[index]);
+  
+price = signal(100);
+weight = signal(5);
+ 
+// computed property
+total = computed(() => {
+  return this.price() * this.weight(); });
 
+constructor() {
+ console.log(this.total());
+ this.price.set(200);
+ console.log(this.total());
+}
   onSelectServer(){
     // updating a signal
     this.selectedServer.set(servers[index]);
   }
-  
-
-
-
 }
