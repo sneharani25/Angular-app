0b822e5be062adbc4188cbe16cd643c6d9be5805
Rani <sneha.d.rani@capgemini.com>
2025-05-15 09:33:45 +0530

    signals and how to use it

--- src/app/assignment-component/success-alert/success-alert.component.ts
+++ src/app/assignment-component/success-alert/success-alert.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import {servers} from '../../../assets/dummy-server'
 
+// It will give the random index value from 0 to the length of the server array.
+const index : number = Math.floor(Math.random() * servers.length);
 @Component({
   selector: 'app-success-alert',
   templateUrl: './success-alert.component.html',
   styleUrl: './success-alert.component.css'
 })
-export class SuccessAlertComponent {
 
-  // It will give the random index value from 0 to the length of the server array.
-  index : number = Math.floor(Math.random() * servers.length);
-  selectedServer = servers[this.index];
+export class SuccessAlertComponent {
+  // initializing a signal
+  selectedServer = signal(servers[index]);
+
+  onSelectServer(){
+    // updating a signal
+    this.selectedServer.set(servers[index]);
+  }
+  
+
+
 
 }
